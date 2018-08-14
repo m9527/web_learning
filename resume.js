a5ec8600800188db9916c19b6ab58274ae10f00d
m9527 <1500712605@qq.com>
2018-08-14 11:20:39 +0800

    Update resume.js

--- resume.js
+++ resume.js
@@ -1,4 +1,21 @@
-function displayDate()
-{
-document.getElementById("demo").innerHTML=Date();
+var date=new Date();
+var hour=date.getHours();
+var min=date.getMinutes();
+var sec=date.getSeconds();
+var str;
+if(hour<5){
+    str="凌晨好！";
+}else if(hour<8){
+    str="早上好！";
+}else if(hour<11){
+    str="上午好！";
+}else if(hour<13){
+    str="中午好！";
+}else if(hour<16){
+    str="下午好！";
+}else if(hour<20){
+    str="晚上好！";
+}else{
+    str="太晚了，休息吧！";
 }
+document.write("现在时间：  "+hour+":"+min+":"+sec+"<br>"+str); 
